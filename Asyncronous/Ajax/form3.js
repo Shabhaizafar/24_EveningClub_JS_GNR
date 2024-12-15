94fe2dec846c1196b9772ed21abda92ac336ad80
Shabhaizafar <zafarshabhai03@gmail.com>
2024-12-15 12:01:18 +0530

    Form

--- Asyncronous/Ajax/form3.js
+++ Asyncronous/Ajax/form3.js
@@ -1,9 +1,42 @@
 const btn = document.querySelector('button');
 
-btn.addEventListener('click',function(event){
-    event.preventDefault();
-    var ans = document.querySelector('input').value.split('\\');
-    console.log(ans);
+// btn.addEventListener('click',function(event){
+//     event.preventDefault();
+//     var ans = document.querySelector('input').;
+//     console.log(ans);
 
-    // location.href = ans[ans.length-1];
+//     // location.href = ans[ans.length-1];
+//     // location.href = "../C_Syllabus.pdf";
+// });
+
+
+
+// const fileInput = document.querySelector('input');
+// fileInput.addEventListener('change', (event) => {
+//     // const file = event.target.files[0]; // Get the first selected file
+//     console.log(event);
+// });
+
+btn.addEventListener('click',function(){
+    var xhttp = new XMLHttpRequest();
+    
+    
+    xhttp.onreadystatechange = function (Data){
+        console.log(Data.currentTarget.readyState);
+    }
+    // xhttp.onprogress = function(){
+    //     console.log("On Progress");
+    // }
+    // xhttp.onload = function(event){  
+    //     // console.log(event);
+    //     console.log("Loading");
+    // };
+    // xhttp.onloadend = function(){
+    //     console.log("On Load End");
+    // }
+    // xhttp.onloadstart = function(){
+    //     console.log("On Load Start");
+    // }
+    xhttp.open("GET","Data.json");
+    xhttp.send();
 });
